bd98571875ede7d34114546c2d154671baad570f
ZellianD <zad8948@rit.edu>
2025-03-27 13:49:11 -0400

    unity should be working better now

--- Semester Project/Holotomy/Assets/Scripts/SceneManager.cs
+++ Semester Project/Holotomy/Assets/Scripts/SceneManager.cs
@@ -1,4 +1,4 @@
-//using LookingGlass;
+using LookingGlass;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,44 +25,44 @@ public class tempSceneChange : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //if (isOnTop) { KeyCheckV2(sceneTop, HardwareButton.Back); }
-        //if (isOnMiddle) { KeyCheckV2(sceneMiddle, HardwareButton.Forward); }
-        //if (isOnBottom) { KeyCheckV2(sceneBottom, HardwareButton.PlayPause); }
+        if (isOnTop) { KeyCheckV2(sceneTop, HardwareButton.Back); }
+        if (isOnMiddle) { KeyCheckV2(sceneMiddle, HardwareButton.Forward); }
+        if (isOnBottom) { KeyCheckV2(sceneBottom, HardwareButton.PlayPause); }
     }
 
-    //private void keyCheck()
-    //{
-    //    if (InputManager.GetButtonDown(HardwareButton.Back))
-    //    {
-    //        if(sceneTop != null)
-    //        {
-    //            SceneManager.LoadScene(sceneTop);
-    //        }
-    //    }
-    //    else if (InputManager.GetButtonDown(HardwareButton.Forward))
-    //    {
-    //        if (sceneMiddle != null)
-    //        {
-    //            SceneManager.LoadScene(sceneMiddle);
-    //        }
-    //    }
-    //    else if (InputManager.GetButtonDown(HardwareButton.PlayPause))
-    //    {
-    //        if(sceneBottom!=null)
-    //        {
-    //            SceneManager.LoadScene(sceneBottom);
-    //        }
-    //    }
-    //}
+    private void keyCheck()
+    {
+        if (InputManager.GetButtonDown(HardwareButton.Back))
+        {
+            if (sceneTop != null)
+            {
+                SceneManager.LoadScene(sceneTop);
+            }
+        }
+        else if (InputManager.GetButtonDown(HardwareButton.Forward))
+        {
+            if (sceneMiddle != null)
+            {
+                SceneManager.LoadScene(sceneMiddle);
+            }
+        }
+        else if (InputManager.GetButtonDown(HardwareButton.PlayPause))
+        {
+            if (sceneBottom != null)
+            {
+                SceneManager.LoadScene(sceneBottom);
+            }
+        }
+    }
 
-    //private void KeyCheckV2(string scene, HardwareButton button)
-    //{
-    //    if(scene != null)
-    //    {
-    //        if(InputManager.GetButtonDown(button))
-    //        {
-    //            SceneManager.LoadScene(scene);
-    //        }
-    //    }
-    //}
+    private void KeyCheckV2(string scene, HardwareButton button)
+    {
+        if (scene != null)
+        {
+            if (InputManager.GetButtonDown(button))
+            {
+                SceneManager.LoadScene(scene);
+            }
+        }
+    }
 }
